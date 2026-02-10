4a1d6256a53e2eb1e3e7e66aa12f06b06c280821
SP Solutions <spsolutionsapps@gmail.com>
2026-02-10 15:30:17 -0300

    agenda libre todo el año pasado

--- src/components/AgendarVisitaModal.svelte
+++ src/components/AgendarVisitaModal.svelte
@@ -191,7 +191,6 @@
           type="date"
           bind:value={formData.scheduled_date}
           required
-          min={getFechaActual()}
           class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary-500 focus:border-transparent"
         />
       </div>
